2a6da43d0a6cdf44d01097f5da1487decc3818af
Tim Jones <tim@spotahome.com>
2018-11-08 13:22:53 +0100

    Add Cobra for command line interface

--- cmd/slip/main.go
+++ cmd/slip/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "slip",
+	Short: "slipway is a manager od docker for local development",
+	Long: `A tool to help make the most of Docker when developing
+projects locally`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("slipway")
+	},
+}
 
 func main() {
-	fmt.Println("slipway")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
